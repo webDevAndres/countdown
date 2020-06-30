2347190876e212045341b20d10d5d28f5e91016c
webdevandres <maciasandres02@gmail.com>
2020-06-30 12:48:52 -0500

    add validation

--- countdown.js
+++ countdown.js
@@ -1,9 +1,44 @@
 "use strict";
 
-var $ = function(id) {
+var $ = function (id) {
     return document.getElementById(id);
 };
 
-window.onload = function() {
+var calculateDays = function () {
+    var event = $("event").value;
+    var dt = $("date").value;
+    var msg = $("message").firstChild;
 
+    if (event.length == 0 || dt.length == 0) {
+        msg.nodeValue = "Please enter both a name and a date";
+        // return;
+    }
+
+    if (dt.indexOf("/") == -1) {
+        msg.nodeValue = "Please enter the date in MM/DD/YYYY format.";
+        // return;
+    }
+
+    var year = dt.substring(dt.length - 3);
+    if (isNaN(year)) {
+        msg.nodeValue = "Please enter the date in MM/DD/YYYY format.";
+        // return;
+    }
+
+    // converts the event date string into a date object
+    var date = new Date(dt);
+    if (date == "Invalid Date") {
+        msg.nodeValue = "Please enter the date in MM/DD/YYYY format.";
+        // return;
+    }
+
+    // calculate days 
+    
+};
+
+
+
+window.onload = function () {
+    $("countdown").onclick = calculateDays;
+    $("event").focus();
 };
