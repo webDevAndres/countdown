46e52d452943331c826c16811777b12b90f0e88a
webdevandres <maciasandres02@gmail.com>
2020-07-05 10:12:01 -0500

    update string length.

--- countdown.js
+++ countdown.js
@@ -11,29 +11,49 @@ var calculateDays = function () {
 
     if (event.length == 0 || dt.length == 0) {
         msg.nodeValue = "Please enter both a name and a date";
-        // return;
+        return;
     }
 
     if (dt.indexOf("/") == -1) {
         msg.nodeValue = "Please enter the date in MM/DD/YYYY format.";
-        // return;
+        return;
     }
 
-    var year = dt.substring(dt.length - 3);
+    var year = dt.substring(dt.length - 4);
     if (isNaN(year)) {
         msg.nodeValue = "Please enter the date in MM/DD/YYYY format.";
-        // return;
+        return;
     }
 
     // converts the event date string into a date object
     var date = new Date(dt);
     if (date == "Invalid Date") {
         msg.nodeValue = "Please enter the date in MM/DD/YYYY format.";
-        // return;
+        return;
     }
 
     // calculate days 
+    var today = new Date();
+    var oneDay = 24*60*60*1000; // hours*min*seconds*ms
+    var days = (date.getTime() - today.getTime()) / oneDay;
+    console.log(date.getTime(), today.getTime());
+    days = Math.ceil(days);
     
+    if (days == 0) {
+        msg.nodeValue = "Hooray! Today is ".concat(event.toLowerCase(),
+        "!\n(", date.toDateString(), ")");
+    }
+
+    if (days < 0) {
+        event = event.substring(0,1).toUpperCase() + event.substring(1);
+        msg.nodeValue = event.concat(" happened ", Math.abs(days),
+        " day(s) ago. \n (", date.toDateString(), ")");
+    }
+
+    if (days > 0) {
+        msg.nodeValue = days.toString().concat(" day(s) until ",
+        event.toLowerCase(), "!\n(", date.toDateString(), ")");
+    }
 };
 
 
